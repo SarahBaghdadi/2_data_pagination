0e4ec7f8577f93f54120d93bac9d1ba045c80293
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-07-18 13:51:45 -0400

    Build search bar, search function, event handlers

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ let itemsPerPage = 9;
 let studentList = document.querySelector('.student-list');
 let linkList = document.querySelector('.link-list');
 
+function clearHtml(element) {
+   element.innerHTML = '';
+}
+
 function showPage(list, page) {
    let startIndex = page * itemsPerPage - itemsPerPage;
    let endIndex = page * itemsPerPage;
@@ -56,7 +60,7 @@ linkList.addEventListener('click', (e) => {
    if (e.target.type == 'button') {
       document.querySelector('button.active').className = '';
       e.target.className = 'active';
-      let list = data;
+      let list = data; // this line
       showPage(list, e.target.textContent);
    }
 });
@@ -65,3 +69,49 @@ linkList.addEventListener('click', (e) => {
 showPage(data, 1);
 addPagination(data);
 
+
+// Add search bar
+
+let header = document.querySelector('.header');
+header.insertAdjacentHTML('beforeend', 
+`<label for="search" class="student-search">
+  <span>Search by name</span>
+  <input id="search" placeholder="Search by name...">
+  <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+</label>`);
+
+/* 
+`simpleSearch` function
+@param searchInput The search input element.
+@param list The data array containing student information.
+*/
+
+const search = document.querySelector('#search'); // Search input element
+const submit = document.querySelector('.student-search button'); // Search submit button
+
+function simpleSearch(searchInput, list) {
+   clearHtml(studentList);
+   let searchResults = [];
+   for (let i = 0; i < list.length; i++){
+      let fullName = `${list[i].name.first} ${list[i].name.last}`;
+      let searchParamaters = searchInput.value.length != 0 && fullName.toLowerCase().includes(searchInput.value.toLowerCase());
+      if (searchParamaters) {
+         // output new list
+         // list = newList;
+         searchResults.push(list[i]);
+      }
+   }
+   showPage(searchResults, 1);
+   console.log(searchResults.length);
+}
+
+submit.addEventListener('click', (event) => {
+   event.preventDefault();
+   // call simple search on new array
+   simpleSearch(search, data);
+ });
+
+ search.addEventListener('keyup', () => {
+   // call simple search on new array
+   simpleSearch(search, data);
+ });
