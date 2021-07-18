b14c1b49181188f56bf35896e3df10a1f151cd91
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-07-17 23:23:55 -0400

    Improve comments and readability

--- js/script.js
+++ js/script.js
@@ -6,16 +6,18 @@ FSJS Project 2 - Data Pagination and Filtering
  /*
 Set global variables.
 */
+
 let itemsPer = 9; // How many items per page to show.
-const studentList = document.querySelector('.student-list'); // Ul containing student data in lis.
-const linkList = document.querySelector('.link-list'); //Ul containing pagination buttons.
+const studentList = document.querySelector('.student-list'); // Ul element containing student data in lis.
+const linkList = document.querySelector('.link-list'); //Ul element containing pagination buttons.
 
  /*
-`showPage` function
+`renderListItem` function
 Creates and appends the elements needed to display a page of students.
 @param list The data array containing student information.
 @param page The current page number.
 */
+
 function renderListItem(item) {
    const picture = item.picture.large;
    const firstName = item.name.first;
@@ -35,6 +37,12 @@ function renderListItem(item) {
    </li>`);
 }
 
+ /*
+`showPage` function
+Calls renderListItem on current page containing specified number of items per page.
+@param list The data array containing student information.
+@param page The current page number.
+*/
 
 function showPage (list, page){
    let startIndex = page * itemsPer - itemsPer - 1;
@@ -50,6 +58,7 @@ function showPage (list, page){
 /*
 `addPagination` function
 Creates and appends pagination buttons.
+@param list The data array containing student information.
 */
 
 function addPagination (list){
@@ -80,9 +89,9 @@ Pagination event listener.
     }
  });
 
-
-
-// Search bar 
+/* 
+Render search bar.
+*/
 
 const header = document.querySelector('header');
 header.insertAdjacentHTML('beforeend', 
@@ -95,29 +104,37 @@ header.insertAdjacentHTML('beforeend',
    `
 )
 
-// Simple search function
+/* 
+`simpleSearch` function
+@param searchInput The search input element.
+@param list The data array containing student information.
+*/
 
- /* Variables to reference the `input` and search `button` elements */
- const search = document.querySelector('#search');
- const submit = document.querySelector('.student-search button');
+ const search = document.querySelector('#search'); // search input element
+ const submit = document.querySelector('.student-search button'); // search submit button
  
  function simpleSearch (searchInput, list) {
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++){
       let fullName = `${list[i].name.first} ${list[i].name.last}`;
-      if ((searchInput.value.length != 0 && fullName.toLowerCase().includes(searchInput.value.toLowerCase())) || searchInput.value.length == 0) {
+      let searchParamaters = (searchInput.value.length != 0 && fullName.toLowerCase().includes(searchInput.value.toLowerCase())) || searchInput.value.length == 0;
+      if (searchParamaters) {
          renderListItem(list[i]);
      } 
    }
  }
    
- /* submit listener button click */
+ /* 
+ event listener for search button click event
+ */
  submit.addEventListener('click', (event) => {
    event.preventDefault();
    simpleSearch(search, data);
  });
  
- /* submit listener keyup */
+ /* 
+ event listener for search keyup event
+ */
  search.addEventListener('keyup', () => {
    simpleSearch(search, data);
  });
